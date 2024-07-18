04013b83c056d3e34eaf878efff95a8ada8defa5
Kitae's Github <aqua0405@gmail.com>
2024-07-18 22:50:33 +0900

    240718-2250

--- src/main/java/kitae/spring/library/config/MyDataRestConfig.java
+++ src/main/java/kitae/spring/library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package kitae.spring.library.config;
 
 import kitae.spring.library.entity.Book;
+import kitae.spring.library.entity.Message;
 import kitae.spring.library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -17,14 +18,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};    // 지원하지 않는 메소드 배열
         config.exposeIdsFor(Book.class);    // Book 엔티티의 ID 노출
         config.exposeIdsFor(Review.class);  // Review 엔티티의 ID 노출
+        config.exposeIdsFor(Message.class); // Message 엔티티의 ID 노출
 
-        disbaleHttpMethods(Book.class, config, theUnsupportedActions);  // 지원하지 않는 메소드 비활성화
-        disbaleHttpMethods(Review.class, config, theUnsupportedActions);  // 지원하지 않는 메소드 비활성화
+        disableHttpMethods(Book.class, config, theUnsupportedActions);  // 지원하지 않는 메소드 비활성화
+        disableHttpMethods(Review.class, config, theUnsupportedActions);  // 지원하지 않는 메소드 비활성화
+        disableHttpMethods(Message.class, config, theUnsupportedActions); // 지원하지 않는 메소드 비활성화
 
         corsRegistry.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);    // CORS 설정
     }
 
-    private void disbaleHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
         config.getExposureConfiguration()       // 엔티티에 대한 구성 가져오기
                 .forDomainType(theClass)       // 엔티티 클래스 지정
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))         // 단일 지원하지 않는 메소드 비활성화
--- src/main/java/kitae/spring/library/repository/MessageRepository.java
+++ src/main/java/kitae/spring/library/repository/MessageRepository.java
@@ -1,8 +1,11 @@
 package kitae.spring.library.repository;
 
 import kitae.spring.library.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-
+    Page<Message> findByUserEmail(@RequestParam("userEmail")String userEmail, Pageable pageable);
 }
